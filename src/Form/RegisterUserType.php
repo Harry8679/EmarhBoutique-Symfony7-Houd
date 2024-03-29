874312ac05dc5e09af1536642ff0a11484a20629
Harry EMARH <emarh.harry.code@gmail.com>
2024-03-29 16:57:17 +0100

    All form types added (TextType, EmailType and PasswordType)

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +16,18 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('password')
+            ->add('firstName', TextType::class, [
+                'label' => 'Votre prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Votre nom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse email'
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe'
+            ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Inscription'
             ])
