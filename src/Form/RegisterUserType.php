47003a36cf7b2f71ace7580604c22a309b712ca7
Harry EMARH <emarh.harry.code@gmail.com>
2024-04-02 23:18:03 +0200

    Confirmation of password

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -34,10 +35,27 @@ class RegisterUserType extends AbstractType
                     'placeholder' => "Veuillez renseigner votre adresse email"
                 ]
             ])
-            ->add('password', PasswordType::class, [
+            /* ->add('password', PasswordType::class, [
                 'label' => 'Votre mot de passe',
                 'attr' => [
                     'placeholder' => "Veuillez renseigner votre mot de passe"
+                ]
+            ])*/
+            ->add('palinPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options' => [
+                    'label' => 'Votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Choisissez un mot de passe'
+                    ],
+                    'hash_property_path' => 'password'
+                ],
+                'second_options' => [
+                    'label' => 'Votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Veuillez confirmer votre mot de passe'
+                    ]
                 ]
             ])
             ->add('submit', SubmitType::class, [
