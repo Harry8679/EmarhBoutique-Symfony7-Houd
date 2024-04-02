1c70e9a8c9118efa9aa9a806a98cd53d01c269ca
Harry EMARH <emarh.harry.code@gmail.com>
2024-04-02 23:07:21 +0200

    Register a user to Database

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        $form = $this->createForm(RegisterUserType::class);
+        $user = new User();
+
+        $form = $this->createForm(RegisterUserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+        }
 
         return $this->render('register/registration.html.twig', [
             'registerForm' => $form->createView()
